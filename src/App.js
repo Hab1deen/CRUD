7a0dd577820ec910cd94bf1211a75a0f5da5bb6f
ฮาบีดีน บุญสาลี <habideen@MacBook-Air--Habideen.local>
2024-02-20 22:09:04 +0700

    hello world

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Users from './components/Users';
+import UserCreate from './components/UserCreate';
+import UserUpdate from './components/UserUpdate';
+
+function App() {
+  return (
+
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Users/>}/>
+        <Route path="create" element={<UserCreate/>}/>
+        <Route path="update" element={<UserUpdate/>}/>
+      </Routes>
+    </div>
+    
+  );
+}
+
+export default App;
